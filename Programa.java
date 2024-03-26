96d01c79b961ab0d8a55df181aa6736a5615f6d9
GuiKre <guilhermekre@gmail.com>
2024-03-26 16:56:38 -0300

    Menu de Execuções

--- Programa.java
+++ Programa.java
@@ -2,20 +2,25 @@ public class Programa {
 
     public static void main(String[] args){
 
-        //Exercicio01.executar();
-        //Exercicio02.executar();
-        //Exercicio03.executar();
-        //Exercicio04.executar();
-        //Exercicio05.executar();
-        //Exercicio06.executar();
-        //Exercicio07.executar();
-        //Exercicio08.executar();
-        //Exercicio09.executar();
-        //Exercicio10.executar();
-        //Exercicio11.executar();
-        //Exercicio12.executar();
-        //Exercicio13.executar();
-        //Exercicio14.executar();
-        Exercicio15.executar();
+        int num = Prompt.lerInteiro("Digite o número da questão que deseja executar(1 a 15): ");
+        
+        switch (num) {
+            case 1:  Exercicio01.executar(); break;
+            case 2:  Exercicio02.executar(); break;
+            case 3:  Exercicio03.executar(); break;
+            case 4:  Exercicio04.executar(); break;
+            case 5:  Exercicio05.executar(); break;
+            case 6:  Exercicio06.executar(); break;
+            case 7:  Exercicio07.executar(); break;
+            case 8:  Exercicio08.executar(); break;
+            case 9:  Exercicio09.executar(); break;
+            case 10: Exercicio10.executar(); break;
+            case 11: Exercicio11.executar(); break;
+            case 12: Exercicio12.executar(); break;
+            case 13: Exercicio13.executar(); break;
+            case 14: Exercicio14.executar(); break;
+            case 15: Exercicio15.executar(); break;
+            default: Prompt.imprimir("Número da questão inválido."); break;      
+        }
     }
 }
